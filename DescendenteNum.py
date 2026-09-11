95321c44490c6856003c13e197905c645c36dc35
Usuario Generico <usuario@git.local>
2026-09-10 21:06:40 -0500

    cargaprueba

---
+++ DescendenteNum.py
@@ -0,0 +1,12 @@
+#organización de Números Desc
+val=[1,7,8,9,3]
+aux=0
+for i in range (0,len(val)):
+   for j  in range (len(val)-1):
+       if val[j]< val[j+1]:
+            aux=val[j]
+            val[j]=val[j+1]
+            val[j+1]=aux
+            print (val[j],val[j+1],aux)
+    
+print (val)
