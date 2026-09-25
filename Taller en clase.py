8603f4d2bc869bd602a0afaa6137509624a5c7d7
omarfegranadosve-crypto <omarfe.granadosve@ecci.edu.co>
2026-09-25 12:40:54 -0500

    Culminación Taller en clase.py
    Terminación Taller en clase 24/9/26

--- Taller en clase.py
+++ Taller en clase.py
@@ -5,3 +5,21 @@
 #Mostrar el promedio general
 #Convertir los nombres a set para validar estudiantes únicos
 #Mostrar los estudiantes registrados sin repetir
+seg="s"
+nomb=[]
+nota=[]
+sum=0
+while (seg=="s"):
+  nomb.append(input("Por favor ingrese el nombre del estudiante a agregar: "))
+  nota.append(float(input("por favor ingrese la nota del estudiante: ")))
+  seg=input("¿Desea agregar otro estudiante? (s/n): ")
+
+for n in nota:
+  sum=sum+n
+prom=sum/len(nota)
+
+print("Los estudiantes ingresados en la lsita son: ",nomb)
+print("El promedio de las notas de los estudiantes anteriormente mostrados es de: ", prom)
+print("Convirtiendo la lista en un set (eliminación de nombres repetidos: )", set(nomb))
+
+#Ejecuta bajo las condiciones descritas
